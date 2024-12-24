35ba1b559e21d5f9822dfd89d1365a95fbad4b6a
Nazia Barveen <nazia.barveen@gmail.com>
2024-12-24 18:49:20 +0530

    Assignment 1- Java DataType

---
+++ src/com/creatio/crm/language/basics/Datatype.java
@@ -0,0 +1,34 @@
+package com.creatio.crm.language.basics;
+
+public class Datatype {
+
+	public static void main(String[] args)
+	{
+		//Datatype used to store simple integer
+		int a = 12;
+		System.out.println(a);
+		
+		//datatype to store decimal number
+		float p = 92.3f;
+		System.out.println(p);
+		
+		//datatype used to store large number of data
+		long m = 95532349792L;
+		System.out.println(m);
+		
+		/* datatype used to store single character
+		 * the value is given within single quotes here.
+		 */
+		char g = 'A';
+		System.out.println(g);
+		
+		//datatype used to store set of characters
+		String name = "Nazia";
+		System.out.println(name);
+		
+		//datatype used to store large decimal number 
+		double t= 35.655;
+		System.out.println(t);
+	}
+	
+}
---
+++ src/com/creatio/crm/language/basics/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.creatio.crm.language.basics;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+	/*
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+ System.out.write(65);
+ System.out.println("hello");
+ System.err.println("hello");
+ Logger.getLogger("MyLogger").info("Error message");
+	}
+
+}
